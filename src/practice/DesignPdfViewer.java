499d6587aef3bf3c8fda23e4bf52c61fe8d2341b
Vipin <vipinvijayan23@gmail.com>
2021-09-10 18:39:37 -0400

    Design Pdf Viewer Solution

--- src/practice/DesignPdfViewer.java
+++ src/practice/DesignPdfViewer.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 public class DesignPdfViewer {
 
 	public static void main(String[] args) {
