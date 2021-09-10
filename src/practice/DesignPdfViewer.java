b8a7a14def0776ad4c9846215717aca83c04ac85
Vipin <vipinvijayan23@gmail.com>
2021-09-10 18:39:10 -0400

    Design Pdf Viewer Solution

---
+++ src/practice/DesignPdfViewer.java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class DesignPdfViewer {
+
+	public static void main(String[] args) {
+		List<Integer> a = Arrays.asList(1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7);
+		Logger.print(designerPdfViewer(a, "abc") + "");
+	}
+
+	public static int designerPdfViewer(List<Integer> h, String word) {
+
+		int maxHeight = 0;
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		int count = 0;
+		for (char character = 'a'; character <= 'z'; character++) {
+			map.put(character, h.get(count));
+			count++;
+		}
+
+		for (char character : word.toCharArray()) {
+			int height = map.get(character);
+			maxHeight = Math.max(maxHeight, height);
+		}
+
+		return maxHeight * word.length();
+
+	}
+}
